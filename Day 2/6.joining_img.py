aed553ff4bdf0099cd9c803d2a4cba6d92556275
nnfuad <nnfuad01@gmail.com>
2026-02-11 02:01:56 +0600

    updated 6 in day2

--- Day 2/6.joining_img.py
+++ Day 2/6.joining_img.py
@@ -3,9 +3,15 @@ import numpy as np
 
 img = cv2.imread("resources/lena.png")
 
-img_hor = np.hstack((img, img))
-img_var = np.vstack((img, img))
+img_hor = np.hstack((img, img)) 
+# np.hstack() is a function in NumPy that horizontally stacks arrays. 
+# In this case, it takes the image 'img' and stacks it with itself horizontally, 
+# resulting in a new image that is twice the width of the original. 
 
+img_var = np.vstack((img, img))
+# np.vstack() is a function in NumPy that vertically stacks arrays. 
+# In this case, it takes the image 'img' and stacks it with itself vertically, 
+# resulting in a new image that is twice the height of the original.
 
 cv2.imshow("Horizontal", img_hor)
 cv2.imshow("Vertical", img_var)
