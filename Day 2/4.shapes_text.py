0816a958a45d4cf73eb0e7ef28fb09a3134d25d8
nnfuad <nnfuad01@gmail.com>
2026-02-11 01:45:42 +0600

    added and updated some files in day2

--- Day 2/4.shapes_text.py
+++ Day 2/4.shapes_text.py
@@ -1,24 +1,27 @@
 import cv2 
 import numpy as np 
 
+# Create a black image (zeros)
 img = np.zeros((512,512,3), np.uint8)
-print(img.shape)
+print(img.shape)  # Should print (512, 512, 3)
 
-img[:] = 255,0,0  # BGR
+# Make entire image blue (BGR format: 255,0,0 = Blue)
+img[:] = (255, 0, 0)  # This will make the whole image blue
 
-# Create a line
-# cv2.line(img, (0,0), (300,400), (0,255,0), 5) # cv2.line(image, start_point, end_point, color, thickness)
+### Create a line (uncomment to use)
+# cv2.line(img, (0,0), (300,400), (0,255,0), 5)  # Green line
 
-### Rectangle
-# cv2.rectangle(img, (100,50), (250, 350), (0,0,255), 7) #cv2.rectangle(image, start_point, end_point, color, thickness)
+### Rectangle (uncomment to use)
+# cv2.rectangle(img, (100,50), (250,350), (0,0,255), 7)  # Red rectangle
 
+### Create circle (uncomment to use)
+# cv2.circle(img, (400,50), 50, (0,0,255), 4)  # Red circle
+# OR filled circle:
+# cv2.circle(img, (400,50), 50, (0,0,255), cv2.FILLED)
 
-### Create circle 
-# cv2.circle(img, (400,50), 50, (0,0,255), 4) # cv2.circle(img, (400,50), 50, (0,0,255), cv2.FILLED)
-
-
-### Put texts
-cv2.putText(img, "InceptionBD", (200,440), cv2.FONT_HERSHEY_COMPLEX,1, (0,255,0), 1)
+### Put text - This should work now!
+cv2.putText(img, "Nur Nafis Fuad", (200,440), cv2.FONT_HERSHEY_DUPLEX, 1, (0,255,0), 1)
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
--- Day 2/5.wrap_prespective.py
+++ Day 2/5.wrap_prespective.py
@@ -9,11 +9,14 @@ img = cv2.imread("resources/cards.jpg")
 
 
 pts1 = np.float32([[752,118],[1120,265],[540,668],[871,830]])
+
 pts2 = np.float32([[0,0], [width, 0], [0,height], [width, height]])
+# This is the destination points for the perspective transform
+# This will map the corners of the card to a flat rectangle of size (width, height)
 
-
-metrix = cv2.getPerspectiveTransform(pts1, pts2)
-img_out = cv2.warpPerspective(img, metrix, (width, height))
+metrix = cv2.getPerspectiveTransform(pts1, pts2) #metrix = cv2.getPerspectiveTransform(pts1, pts2) This function computes the perspective transformation matrix that maps the points in pts1 to the corresponding points in pts2. The resulting matrix can then be used to perform the perspective transformation on the image.
+img_out = cv2.warpPerspective(img, metrix, (width, height)) #img_out = cv2.warpPerspective(img, metrix, (width, height)) Here, metrix is the transformation matrix obtained from cv2.getPerspectiveTransform, and (width, height) specifies the size of the output image.
+# This applies the perspective transformation to the image, resulting in a warped image of the card that is now flat and has the specified width and height.
 
 cv2.imshow('cards', img)
 cv2.imshow('cards_warp', img_out)
